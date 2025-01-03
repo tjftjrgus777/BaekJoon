8ca26c59088aa1bffbfe792f68c80841f4ac0365
tjftjrgus777 <119033033+tjftjrgus777@users.noreply.github.com>
2025-01-03 16:03:54 +0900

    [Bronze V] Title: ??!, Time: 132 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        
+        String s = bf.readLine();
+        String result = s + "??!";
+        
+        System.out.print(result);
+        
+    }
+}
