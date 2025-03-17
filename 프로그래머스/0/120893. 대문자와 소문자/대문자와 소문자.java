dbdddff2ec1d6dd080d03bc9499d6c51636743c9
tjftjrgus777 <119033033+tjftjrgus777@users.noreply.github.com>
2025-03-17 16:16:14 +0900

    [level 0] Title: 대문자와 소문자, Time: 18.87 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,11 +1,10 @@
 class Solution {
     public String solution(String my_string) {
         String answer = "";
-        
-        for(int i = 0; i < my_string.length(); i++) {
-            if(Character.isUpperCase(my_string.charAt(i))) {
+        for(int i = 0; i < my_string.length(); i++){
+            if(Character.isUpperCase(my_string.charAt(i))){
                 answer += Character.toLowerCase(my_string.charAt(i));
-            } else {
+            }else{
                 answer += Character.toUpperCase(my_string.charAt(i));
             }
         }
